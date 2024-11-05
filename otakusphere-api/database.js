3acf6d1a00dc1ecf5f4915b9b42d30a095dc9aab
Noodles <adrianalexis624@gmail.com>
2024-11-04 21:16:03 -0600

    Added more functions to database.js
    Added function to get a single user from the db with their given id
    Added function to create a user with the input username, email, and password

--- otakusphere-api/database.js
+++ otakusphere-api/database.js
@@ -21,9 +21,11 @@ const db = mysql.createPool({
 
 // example of async function with await:
 
+// functions to get all of the users from db
 async function getAllUsers(){
     try{
-        const allUsers = await db.query("SELECT * FROM User;"); // returns an array of a bunch of info. array[0] will be idex of what we need
+        // [allUsers] - brackets will automatically assign to the array[0]
+        const [allUsers] = await db.query("SELECT * FROM User;"); // returns an array of a bunch of info. array[0] will be idex of what we need
         return allUsers
     }
     catch(err){
@@ -32,8 +34,40 @@ async function getAllUsers(){
     }
 }
 
-// [allUsers] - brackets will automatically assign to the array[0]
-const [allUsers] = await getAllUsers();
-console.log(allUsers);
+// function to get only one user from the db specified by their ID
+async function getUser(id) {
+    try {
+        // don't use ${id}, use ? instead. when using ?, make sure to add the array as a parameter in query(...) method
+        const [user] = await db.query(`
+            SELECT * FROM User 
+            WHERE user_id = ?
+            `, [id]);
+
+            return user[0]; // we can return user[0], since the array will only have the 1 user in it we queried
+    }
+    catch(err) {
+        console.log("ERROR: getUser(id) could not retrieve user");
+    }
+}
+
+// function to insert a user into the db
+async function createUser(username, email, password){
+    const [result] = await db.query(`
+        INSERT INTO User(username, email, password)
+        VALUES (?, ?, ?)`, [username, email, password]);
+
+    return result.insertId; // the insert returned an object, and now we're returning it's insertId attribute - the user_id value it was given
+}
 
 
+// const allUsers = await getAllUsers();
+// console.log(allUsers);
+
+// const getUserTest = await getUser(1);
+// console.log(getUserTest);
+
+// const createUserTest = await createUser("TESTING3", "tester3@test.com", "testPassword");
+// console.log(createUserTest);
+
+// const allUsers = await getAllUsers();
+// console.log(allUsers);
