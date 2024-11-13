22bc6f55c8764ba828c3f8df1ada22eab8bf5fe0
Kiahna Isadore <kiahnaisadore@gmail.com>
2024-11-13 13:04:24 -0600

    you can now test the dropdown in auth.js

--- otakusphere-ui/js/auth.js
+++ otakusphere-ui/js/auth.js
@@ -7,23 +7,39 @@ function updateAuthUI() {
         // logged in
         authSection.innerHTML = `
             <div class="dropdown">
-                <button class="btn btn-link text-light dropdown-toggle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="fas fa-user-circle fa-lg"></i>
-                </button>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-                    <li><a class="dropdown-item" href="profile.html">Profile</a></li>
-                    <li><a class="dropdown-item" href="settings.html">Settings</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#" onclick="logout()">Sign Out</a></li>
-                </ul>
-            </div>
-        `;
+            <button class="btn btn-link text-light dropdown-toggle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="fas fa-user-circle fa-lg"></i>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                <li><a class="dropdown-item" href="watchlist.html">Watchlist</a></li>
+                <li><a class="dropdown-item" href="profile.html">Profile</a></li>
+                <li><a class="dropdown-item" href="settings.html">Settings</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#" onclick="logout()">Sign Out</a></li>
+            </ul>
+        </div>
+    `;
     } else {
         // not logged in
         authSection.innerHTML = `
             <button onclick="window.location.href='login.html'" class="btn btn-outline-light me-2 custom-signin-btn">Sign In</button>
             <button onclick="window.location.href='register.html'" class="btn btn-primary custom-signup-btn">Sign Up</button>
-`;
+            `;
+            // UNCOMMENT AND COMMENT OUT ABOVE CODE TO SEE DROPDOWN MENU
+            
+    //         <div class="dropdown">
+    //         <button class="btn btn-link text-light dropdown-toggle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+    //             <i class="fas fa-user-circle fa-lg"></i>
+    //         </button>
+    //         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+    //             <li><a class="dropdown-item" href="watchlist.html">Watchlist</a></li>
+    //             <li><a class="dropdown-item" href="profile.html">Profile</a></li>
+    //             <li><a class="dropdown-item" href="settings.html">Settings</a></li>
+    //             <li><hr class="dropdown-divider"></li>
+    //             <li><a class="dropdown-item" href="#" onclick="logout()">Sign Out</a></li>
+    //         </ul>
+    //     </div>
+    // `;
     }
 }
 
